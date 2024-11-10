c2976159f0217180456c96e2a72f099f3b87a4ed
Sibulele Ngada <sibulelengada@gmail.com>
2024-11-10 06:11:26 +0200

    Execute user stories

--- scripts.js
+++ scripts.js
@@ -3,9 +3,15 @@ import { add, subtract, reset } from "./model/actions.js";
 
 subscribe((_, next) => console.log(next));
 
+// SCENARIO 1: Initial State Verification
 console.log(getState());
 
+// SCENARIO 2: Incrementing the Counter
 dispatch(add());
 dispatch(add());
+
+// SCENARIO 3: Decrementing the Counter
 dispatch(subtract());
+
+// SCENARIO 4: Resetting the Counter
 dispatch(reset());
