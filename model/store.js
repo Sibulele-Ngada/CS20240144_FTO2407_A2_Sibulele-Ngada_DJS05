e2b42d53e41731882988660687108e55ef5250aa
Sibulele Ngada <sibulelengada@gmail.com>
2024-11-10 05:59:02 +0200

    Store documentation

--- model/store.js
+++ model/store.js
@@ -1,22 +1,18 @@
-import { Action } from "./actions.js";
-import { reducer } from "./reducers.js";
-
 /**
- * @typedef {object} Tally
- */
-
-/**
+ * State of Tally App
  * @typedef {object} State
  * @prop {number} count
  */
 export const State = {};
 
 /**
+ * View current state of Tally App
  * @callback GetState
  * @returns {State}
  */
 
 /**
+ * Sends action to reducer
  * @callback Dispatch
  * @param {Action} action
  */
@@ -30,46 +26,3 @@ export const State = {};
  * @param {State} prev
  * @param {State} next
  */
-
-/**
- * @type {Array<Subscription>}
- */
-let subscribers = [];
-
-/**
- * @type {Array<State>}
- */
-let states = [{ count: 0 }];
-
-/**
- * @type {GetState}
- */
-export const getState = () => Object.freeze({ ...states[0] });
-
-/**
- * @type {Dispatch}
- */
-export const dispatch = (action) => {
-  const prev = getState();
-  const next = reducer(prev, action);
-  subscribers.forEach((subscriber) => subscriber(prev, next));
-  states.unshift(next);
-};
-
-/**
- *
- * @param {Subscription} subscription
- * @returns {EmptyFn}
- */
-export const subscribe = (subscription) => {
-  subscribers.push(subscription);
-
-  const unsubscribe = () => {
-    const newSubscribers = subscribers.filter(
-      (subscriber) => subscriber !== subscription
-    );
-    subscribers = newSubscribers;
-  };
-
-  return unsubscribe;
-};
