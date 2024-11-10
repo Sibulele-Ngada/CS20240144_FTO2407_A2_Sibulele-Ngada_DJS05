7b8fbf9c075df2ea47e29720e26dec30cf76c5d7
Sibulele Ngada <sibulelengada@gmail.com>
2024-11-10 06:02:01 +0200

    Dispatch function logic

--- model/store.js
+++ model/store.js
@@ -41,3 +41,31 @@ let states = [{ count: 0 }];
  * @type {GetState}
  */
 export const getState = () => Object.freeze({ ...states[0] });
+
+/**
+ * @type {Dispatch}
+ */
+export const dispatch = (action) => {
+  const prev = getState();
+  const next = reducer(prev, action);
+  subscribers.forEach((subscriber) => subscriber(prev, next));
+  states.unshift(next);
+};
+
+/**
+ *
+ * @param {Subscription} subscription
+ * @returns {EmptyFn}
+ */
+export const subscribe = (subscription) => {
+  subscribers.push(subscription);
+
+  const unsubscribe = () => {
+    const newSubscribers = subscribers.filter(
+      (subscriber) => subscriber !== subscription
+    );
+    subscribers = newSubscribers;
+  };
+
+  return unsubscribe;
+};
